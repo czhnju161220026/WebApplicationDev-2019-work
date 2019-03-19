7faa9873935f76915f3e7420b82ee09720806bb9
czhnju161220026 <997520420@qq.com>
2019-03-19 22:09:07 +0800

    使用css进行弹窗提示，对比signup中的js弹窗

--- src/main/resources/static/js/log_in.js
+++ src/main/resources/static/js/log_in.js
@@ -1,8 +1,10 @@
 function judge() {
     document.getElementById("window").classList.remove("hide");
+    document.getElementById("background").classList.add("blur");
     return false;
 }
 
 function retype() {
     document.getElementById("window").classList.add("hide");
+    document.getElementById("background").classList.remove("blur");
 }
